baa36f3d4545d8db250c62a7d249e3d86b9b92e7
john speny <johnspeny@gmail.com>
2023-03-29 11:35:55 +0300

    Add spdlog for logging

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 #include <array-based-lists/array_stack.h>
 #include <nanobench.h>
+#include "spdlog/spdlog.h"
 
 int main()
 {
+
+	spdlog::info("Welcome to spdlog!");
+
 	ArrayStack obj;
 
 	std::cout << "Welcome" << "\n";
